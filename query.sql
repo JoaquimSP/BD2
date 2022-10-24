a60f6b7ba5f5d255084bdae0013f7b78db51d333
Joaquim Pinto <54048827+JoaquimSP@users.noreply.github.com>
2022-10-24 20:56:46 -0300

    adding the queries

---
+++ query.sql
@@ -0,0 +1,22 @@
+/* Exercicio 1*/
+SELECT * FROM employees;
+
+/* Exercicio 2 */
+SELECT emp_no, first_name FROM employees;
+
+/* Exercicio 3 */
+SELECT DISTINCT first_name FROM employees;
+
+/* Exercicio 4 */
+SELECT DISTINCT first_name FROM employees ORDER BY first_name;
+
+/* Exercicio 5 */
+SELECT DISTINCT first_name FROM employees ORDER BY first_name DESC;
+
+/* Exercicio 6 */
+SELECT DISTINCT first_name FROM employees ORDER BY first_name ASC;
+
+/* Exercicio 7 */
+SELECT DISTINCT first_name, last_name FROM employees;
+
+/* Exercicio 8 */
