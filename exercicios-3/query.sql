aa8807fe4cc6ccf131042e636ff6368eb25367bd
Joaquim Pinto <54048827+JoaquimSP@users.noreply.github.com>
2022-11-14 22:27:56 -0300

    new commit

---
+++ exercicios-3/query.sql
@@ -0,0 +1,8 @@
+SELECT * FROM country;
+
+/* Conte o total de países */
+SELECT COUNT(*) AS total FROM country;
+
+/* Retorne o nome dos continentes e o total de países que neles existem */
+/* SELECT continent */
+SELECT name, continent FROM country;
