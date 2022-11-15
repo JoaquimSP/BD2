680ce6b435369a51cfe56be5c963072a65cc84f8
Joaquim Pinto <54048827+JoaquimSP@users.noreply.github.com>
2022-11-14 22:34:45 -0300

    update

--- exercicios-3/query.sql
+++ exercicios-3/query.sql
@@ -1,8 +1,13 @@
 SELECT * FROM country;
 
-/* Conte o total de países */
+/* 1 - Conte o total de países */
 SELECT COUNT(*) AS total FROM country;
 
-/* Retorne o nome dos continentes e o total de países que neles existem */
-/* SELECT continent */
+/* 2 - Retorne o nome dos continentes e o total de países que neles existem */
 SELECT name, continent FROM country;
+
+/* 3 - Retorne o nome dos continentes e o total de países que neles existem. Ordene o resultado em
+ordem alfabética pelo nome do continente. */
+
+/* 4 - Retorne o nome dos continentes e o total de países que nele existem. Filtre os continentes que 
+possuem mais que 50 países */
