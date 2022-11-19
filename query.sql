191540bd635d37e80ce34df88f77c2f9148a107a
Joaquim Pinto <54048827+JoaquimSP@users.noreply.github.com>
2022-11-18 22:25:23 -0300

    new commit

--- query.sql
+++ query.sql
@@ -1,22 +1,65 @@
-/* Exercicio 1*/
-SELECT * FROM employees;
+SELECT * FROM country;
 
-/* Exercicio 2 */
-SELECT emp_no, first_name FROM employees;
+/* Utilizando a base de dados WORLD, resolva os seguintes exercícios. */
 
-/* Exercicio 3 */
-SELECT DISTINCT first_name FROM employees;
+/* 1 - Conte o total de países */
+SELECT COUNT(*) AS total FROM country;
 
-/* Exercicio 4 */
-SELECT DISTINCT first_name FROM employees ORDER BY first_name;
+/* 2 - Retorne o nome dos continentes e o total de países que neles existem */
+SELECT name, continent FROM country;
 
-/* Exercicio 5 */
-SELECT DISTINCT first_name FROM employees ORDER BY first_name DESC;
+/* 3 - Retorne o nome dos continentes e o total de países que neles existem. Ordene o resultado em
+ordem alfabética pelo nome do continente. */
+SELECT country.Continent, COUNT(country.name) AS NumberCountries FROM world.country GROUP BY country.Continent ORDER BY country.Continent;
 
-/* Exercicio 6 */
-SELECT DISTINCT first_name FROM employees ORDER BY first_name ASC;
+/* 4 - Retorne o nome dos continentes e o total de países que nele existem. Filtre os continentes que possuem mais que 50 países. */
+SELECT country.Continent, COUNT(country.name) AS NumberCountries FROM world.country GROUP BY country.Continent HAVING COUNT(*) > 50;
 
-/* Exercicio 7 */
-SELECT DISTINCT first_name, last_name FROM employees;
+/* 5 - Retorne a área do maior país. */
+SELECT country.SurfaceArea FROM world.country ORDER BY country.SurfaceArea DESC limit 1;
 
-/* Exercicio 8 */
+/* 6 - Retorne a área do menor país. */
+SELECT country.SurfaceArea FROM world.country ORDER BY country.SurfaceArea limit 1;
+
+/* 7 - Retorne a média da área de todos os países. */
+SELECT AVG(country.SurfaceArea) AS Media FROM country;
+
+/* 8 - Retorne a média da área dos países da Europa. */
+SELECT AVG(country.SurfaceArea) as Media FROM country WHERE country.Continent LIKE 'Europe';
+
+/* 9 - Retorne o nome do continente e a média da área dos países (que fazem parte do continente).  */
+SELECT country.Continent, AVG(country.SurfaceArea) AS Media FROM world.country GROUP BY country.Continent;
+
+/* 10 -  Retorne o nome do continente e a média da área dos países (que fazem parte do continente). 
+Ordene pela média da área de maior a menor. */
+SELECT country.Continent, AVG(country.SurfaceArea) AS Media FROM world.country GROUP BY country.Continent ORDER BY Media DESC;
+
+/* 11 - */
+
+/* 12 - */
+
+/* 13 - */
+
+/* 14 - */
+
+/* 15 - */
+
+/* 16 - */
+
+/* 17 - */
+
+/* 18 - */
+
+/* Utilizando a base de dados SAKILA, resolva os seguintes exercícios.  */
+
+/* 19 - */
+
+/* 20 */
+
+/* 21 */
+
+/* 22 */
+
+
+
+
